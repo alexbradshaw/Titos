a77f2b544f7f8fbb6cc2551fe3d8fd0484023dfe
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-08-02 02:21:45 -0400

    added links and logo

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css';
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+
+    return (
+        <div className='header'>
+            <Link to='/' className='logo'>
+                
+            </Link>
+            <div className='headerContainer'>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+            </div>
+        </div>
+    );
+
+};
+
+export default Header;
