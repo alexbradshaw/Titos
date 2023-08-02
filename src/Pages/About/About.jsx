880925e157c35108c5e8deb03412c18204e2903a
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-08-02 02:22:33 -0400

    Started about

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,16 @@
+import './About.css';
+
+const About = () => {
+
+    return (
+        <div className='about'>
+            <div className='aboutContainer'>
+                <a>About</a>
+                <a>About</a>
+            </div>
+        </div>
+    );
+
+};
+
+export default About;
