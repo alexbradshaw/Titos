f493c02fcc553557ab6aac1d486f5a9b9805b0c0
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-08-02 02:20:54 -0400

    added icons to footer, each links to a social

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import './Footer.css';
+import { Icon } from '@iconify/react';
+
+const icons = [
+    {"social":"mingcute:phone-fill", "link":"tel:+17063513376"},
+    {"social":"mi:email", "link":"mailto:titogerman35@yahoo.com"},
+    {"social":"uiw:facebook", "link":"https://www.facebook.com/tito.suarez"},
+    {"social":"uil:instagram-alt", "link":"https://www.instagram.com/titoakagerman_thebarber"},
+    {"social":"cib:cashapp", "link":"https://cash.app/suarezgerm18"},
+    {"social":"ion:logo-venmo", "link":"https://account.venmo.com/u/German_suarez_2"},
+]
+
+const AnIcon = (props) => {
+    return (
+        <a href={props.link} target='_blank' key={props.index + "a"} > 
+            <Icon icon={props.social} key={props.index} width={40}/>
+        </a>
+    );
+}
+
+const Footer = () => {
+
+    return (
+        <div className='footer'>
+            <div className='socials foot'>
+                <div className='socialsContainer'>
+                    {icons.map((icon, index)=> {
+                        return (
+                            <AnIcon link={icon.link} social={icon.social} index={index + 'icon'} key={index + 'component'}/>
+                        )
+                        })}
+                </div>
+            </div>
+        </div>
+    );
+
+};
+
+export default Footer;
