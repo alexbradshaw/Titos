a7e9cef5f2ba3747cc6d94425db678a8f63367ab
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-08-02 23:58:18 -0400

    created separate component for icon

---
+++ src/Components/AnIcon/AnIcon.jsx
@@ -0,0 +1,12 @@
+import './AnIcon.css'
+import { Icon } from '@iconify/react';
+
+const AnIcon = (props) => {
+    return (
+        <a className='iconA' href={props.link} target='_blank' key={props.index + "a"} > 
+            <Icon icon={props.social} key={props.index} width={40}/>
+        </a>
+    );
+}
+
+export default AnIcon;
