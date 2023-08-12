b6d5909087031d5175eeee4b1e5a402c853028d6
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-08-12 16:47:48 -0400

    changing social media to be one global state variable

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,6 @@
 import './Footer.css';
 import { Icon } from '@iconify/react';
 
-const icons = [
-    {"social":"mingcute:phone-fill", "link":"tel:+17063513376"},
-    {"social":"mi:email", "link":"mailto:titogerman35@yahoo.com"},
-    {"social":"uiw:facebook", "link":"https://www.facebook.com/tito.suarez"},
-    {"social":"uil:instagram-alt", "link":"https://www.instagram.com/titoakagerman_thebarber"},
-    {"social":"cib:cashapp", "link":"https://cash.app/suarezgerm18"},
-    {"social":"ion:logo-venmo", "link":"https://account.venmo.com/u/German_suarez_2"},
-]
-
 const AnIcon = (props) => {
     return (
         <a href={props.link} target='_blank' key={props.index + "a"} > 
@@ -18,13 +9,12 @@ const AnIcon = (props) => {
     );
 }
 
-const Footer = () => {
-
+const Footer = (props) => {
     return (
         <div className='footer'>
             <div className='socials foot'>
                 <div className='socialsContainer'>
-                    {icons.map((icon, index)=> {
+                    {props.socials.map((icon, index)=> {
                         return (
                             <AnIcon link={icon.link} social={icon.social} index={index + 'icon'} key={index + 'component'}/>
                         )
