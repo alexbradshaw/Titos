e8fbfd5b8084df7781520f5d37908c533f3c9978
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-08-18 17:36:27 -0400

    added a few aria labels, updated component to be semantic

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,15 +5,13 @@ import { Link } from "react-router-dom";
 const Header = () => {
 
     return (
-        <div className='header'>
-            <Link to='/' className='logo'>
-                
-            </Link>
+        <header className='header' aria-label='header'>
+            <Link to='/' className='logo' aria-label='Logo link to Home'></Link>
             <div className='headerContainer'>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
             </div>
-        </div>
+        </header>
     );
 
 };
