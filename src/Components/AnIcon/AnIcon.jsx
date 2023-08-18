8059cd98cf2ea211da9e8b8485d82718f0c54191
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-08-18 17:33:51 -0400

    added aria labels to icons

--- src/Components/AnIcon/AnIcon.jsx
+++ src/Components/AnIcon/AnIcon.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const AnIcon = (props) => {
     return (
-        <a className='iconA' href={props.link} target='_blank' key={props.index + "a"} > 
+        <a className='iconA' href={props.link} target='_blank' key={props.index + "a"} aria-label={props.alt}> 
             <Icon icon={props.social} key={props.index} width={40}/>
         </a>
     );
