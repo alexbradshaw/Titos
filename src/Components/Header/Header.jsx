516b496b61e6ed9aa8d0b9d901d7301ddb2ad644
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-09-04 10:23:35 -0400

    turned header links into an array, also added active class toggle from useLocation rrd hook

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,34 @@
 import './Header.css';
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+const HeaderContainer = () => {
+    
+    const links = [
+        {"to":"/", "name":"Home"},
+        {"to":"/about", "name":"About"},
+    ]
+
+    console.log(useLocation().pathname);
+
+    return (
+        <div className='headerContainer'>
+            {
+                links.map((link, index)=>{
+                    return <Link to={link.to} key={"link "+ index} className={useLocation().pathname == link.to ? "active" : ""}>{link.name}</Link>
+                })
+            }
+        </div>
+    );
+};
 
 const Header = () => {
-
     return (
         <header className='header' aria-label='header'>
             <Link to='/' className='logo' aria-label='Logo link to Home'></Link>
-            <div className='headerContainer'>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-            </div>
+            <HeaderContainer/>
         </header>
     );
-
 };
 
 export default Header;
